a06b2196289a77718bfa6b8b83c79f1aea2cfa72
CaixyPromise <caixypromised@gmail.com>
2024-01-26 18:26:33 +0800

    v1.0发布: 完成第一版开发，已完成各项单元测试

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {Provider} from "react-redux";
 import {RouterProvider} from "react-router-dom";
 import store from "./store/index";
 import router from "./router";
+// current version: 1.0
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
